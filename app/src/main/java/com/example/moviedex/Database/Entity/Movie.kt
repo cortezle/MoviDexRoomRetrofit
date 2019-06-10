2c77b3100b6cc643b57725aef4c5db95718ad056
cortezle <00029117@uca.edu.sv>
2019-06-09 21:10:15 -0600

    se removio field a los atributos del modelo

--- app/src/main/java/com/example/moviedex/Database/Entity/Movie.kt
+++ app/src/main/java/com/example/moviedex/Database/Entity/Movie.kt
@@ -3,50 +3,23 @@ package com.example.moviedex.Database.Entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.squareup.moshi.Json
 
-@Entity(tableName = "movies")
+@Entity
 data class Movie (
-
     @PrimaryKey
-    @field:Json(name="imdbID")
     val imdbID: String = "N/a",
-
-    @field:Json(name="Title")
     val Title:String = "N/A",
-
-    @field:Json(name="Year")
     val Year:String = "N/A",
-
-    @field:Json(name="Released")
     val Released: String = "N/A",
-
-    @field:Json(name="Runtime")
     val Runtime:String = "N/A",
-
-    @field:Json(name="Genre")
     val Genre:String = "N/A",
-
-    @field:Json(name="Director")
     val Director:String = "N/A",
-
-    @field:Json(name="Actors")
     val Actors:String = "N/A",
-
-    @field:Json(name="Plot")
     val Plot:String = "N/A",
-
-    @field:Json(name="Language")
     val Language:String = "N/A",
-
-    @field:Json(name="imdbRating")
     val imdbRating:String = "N/A",
-
-    @field:Json(name="Poster")
     val Poster:String = "N/A"
 )
-
-
 
 data class MoviePreview(
     val Title: String = "N/A",
